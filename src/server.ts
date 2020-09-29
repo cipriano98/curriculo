c748fc84c9768941e6f74da25a44c7cb3a53a52e
cipriano98 <natancipriano98@gmail.com>
2020-09-29 17:39:00 -0300

    Ajustes

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { RoutesMapper } from '@nestjs/core/middleware/routes-mapper';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use('/api', RoutesMapper)
 
   const port = process.env.PORT || 3333
   await app.listen(port, '0.0.0.0', () => {
