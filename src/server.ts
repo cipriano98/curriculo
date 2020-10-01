38f3a4ac5b4a4f99b559944fc60d5f71d821bac3
cipriano98 <natancipriano98@gmail.com>
2020-10-01 11:10:44 -0300

    Ajustes

--- src/server.ts
+++ src/server.ts
@@ -4,14 +4,14 @@ import { AppModule } from './app.module';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
-    const port = process.env.PORT || 3333
-    await app.listen(3333, '0.0.0.0', () => {
-        // console.clear()
-        console.log(`\n${process.env.npm_package_NAME} is running in http://localhost:${port}`)
-        // console.log(process.env.npm_package_DESCRIPTION)
-        console.log(`${new Date().toLocaleDateString()} - ${new Date().toLocaleTimeString()}\n`);
-    });
-    //   app.listenAsync(port, '0.0.0.0');
+    // const port = process.env.PORT || 3333
+    // await app.listen(port, '0.0.0.0', () => {
+    //     console.clear()
+    //     console.log(`\n${process.env.npm_package_NAME} is running in http://localhost:${80}`)
+    //     console.log(process.env.npm_package_DESCRIPTION)
+    //     console.log(`${new Date().toLocaleDateString()} - ${new Date().toLocaleTimeString()}\n`);
+    // });
+      app.listenAsync(80, '0.0.0.0');
 }
 bootstrap()
 
