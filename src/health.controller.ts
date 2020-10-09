6d7bc797afab14054d1dd80d13f66f4e80d73163
cipriano98 <natancipriano98@gmail.com>
2020-10-09 14:45:54 -0300

    Ajustes

--- src/health.controller.ts
+++ src/health.controller.ts
@@ -1,20 +1,30 @@
-import { Controller, Dependencies, Get } from '@nestjs/common';
+import { Controller, Dependencies, Get, HttpCode, HttpStatus } from '@nestjs/common';
 import { HealthCheckService, DNSHealthIndicator, HealthCheck } from '@nestjs/terminus';
 @Controller('health')
 @Dependencies(HealthCheckService, DNSHealthIndicator)
 export class HealthController {
-  constructor(
-    private health,
-    private dns,
-  ) { }
+    constructor(
+        private health,
+        private dns,
+    ) { }
 
-  @Get()
-  @HealthCheck()
-  healthCheck() {
-    const port = process.env.PORT || 3333
-    return this.health.check([
-      async () => await this.dns.pingCheck('my-server', `http://localhost:${port}/user`),
-      // async () => this.dns.pingCheck('zeta-analise', `http://zeta08primusweb.com.br:${3017}/api/v1/health`),
-    ])
-  }
+    @Get()
+    @HttpCode(HttpStatus.OK)
+    @HealthCheck()
+    healthCheck() {
+        const port = process.env.PORT || 3333
+        return this.health.check([
+            async () =>  await this.dns.pingCheck(process.env.npm_package_NAME, `http://localhost:${port}/api/v1/health/status`),
+            async () => this.dns.pingCheck('zeta-analise', `http://zeta08.primusweb.com.br:${3017}/api/v1/health`),
+        ])
+    }
+    
+    @Get('status')
+    @HttpCode(HttpStatus.OK)
+    status() {
+        return {
+            server: process.env.npm_package_NAME,
+            up: true
+        }
+    }
 }
