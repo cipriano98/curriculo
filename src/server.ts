670725ab7c9bb240827409ed2312bf1d24293b30
cipriano98 <natancipriano98@gmail.com>
2020-10-20 15:59:55 -0300

    Romeve chalk

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { UnlessMiddleware } from './middleware/router/unless.middleware';
 import { TokenMiddleware } from './middleware/token/token.middleware';
-import chalk = require('chalk')
+// import chalk = require('chalk')
 
 /**
  * Prefixo global
@@ -55,7 +55,7 @@ export class Server {
         const server = await app.listen(process.env.PORT || 3000, '0.0.0.0', () => {
             console.clear()
             // console.log(`\n[${chalk.bold.hex('#28f000')(process.env.npm_package_NAME.toUpperCase())}] is running in ${chalk.blue.underline(`http://localhost:${server.address().port + globalPrefix}`)}`)
-            console.log(`\n[${chalk.bold.hex('#28f000')(process.env.npm_package_NAME)}] is running in ${chalk.blue.underline(`http://localhost:${server.address().port + globalPrefix}`)}`)
+            // console.log(`\n[${chalk.bold.hex('#28f000')(process.env.npm_package_NAME)}] is running in ${chalk.blue.underline(`http://localhost:${server.address().port + globalPrefix}`)}`)
             console.log(process.env.npm_package_DESCRIPTION)
             console.log(`${new Date().toLocaleDateString()} - ${new Date().toLocaleTimeString()}\n`);
         });
