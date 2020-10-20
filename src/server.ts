65b8429906bf164452b60c6d72ecb66fe339561a
cipriano98 <natancipriano98@gmail.com>
2020-10-20 12:51:41 -0300

    ajustes

--- src/server.ts
+++ src/server.ts
@@ -52,7 +52,7 @@ export class Server {
             `${globalPrefix}/health/status`,
         ))
 
-        const server = await app.listen(process.env.PORT || 3333, '0.0.0.0', () => {
+        const server = await app.listen(process.env.PORT || 3000, '0.0.0.0', () => {
             console.clear()
             console.log(`\n[${chalk.bold.hex('#28f000')(process.env.npm_package_NAME.toUpperCase())}] is running in ${chalk.blue.underline(`http://localhost:${server.address().port + globalPrefix}`)}`)
             console.log(process.env.npm_package_DESCRIPTION)
