0898d905053be43f58fd2a71592fc087a65a9f6e
cipriano98 <natancipriano98@gmail.com>
2020-11-16 16:05:46 -0300

    Ajustes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post, Res } from '@nestjs/common'
-import { UserService } from '../user/user.service'
 import { User } from '@prisma/client'
+
+import { UserService } from '../user/user.service'
 
 import jwt = require('jsonwebtoken')
 
@@ -30,57 +31,60 @@ export class AuthController {
             return res.status(400).json({ auth: false, message: 'Os campos devem ser preenchidos corretamente' });
         }
 
-            let existsUser = {};
-            try {
-                if (data.email == process.env.ADMIN_EMAIL && data.secret == process.env.ADMIN_SECRET) {
-                    existsUser['email'] = 'admin@curriculounico.com.br'
-                    existsUser['role'] = 'ADMIN'
-                    existsUser['cpf'] = '84753340082'
-                    existsUser['fullname'] = 'ADMIN'
-                    existsUser['preferencialname'] = 'ADMIN'
-                    existsUser['secret'] = bcrypt.hashSync(data.secret, 10);
-                    existsUser['id'] = 'ADMIN';
-                } else {
-                    existsUser = await this.userService.getByEmail(data.email);
-                }
-                if (existsUser && existsUser['email'] != null) {
-                    if (await bcrypt.compare(data.secret, existsUser['secret'])) {
-                        delete existsUser['secret']
-                        const secret = process.env.SERVER_SECRET_TOKEN || 'Currículo→Único';
-                        const token = jwt.sign({
-                            id: existsUser['id'],
-                            email: existsUser['email'],
-                            role: existsUser['role'],
-                            name: existsUser['preferencialname'] || existsUser['nickname'] || existsUser['fullname'],
-                        }, secret, { expiresIn: '2h' });
+        let existsUser: User
+        try {
+            if (data.email == process.env.ADMIN_EMAIL && data.secret == process.env.ADMIN_SECRET) {
+                existsUser.email = 'admin@curriculounico.com.br'
+                existsUser.role = 'ADMIN'
+                existsUser.cpf = '84753340082'
+                existsUser.fullname = 'Administrator'
+                existsUser.preferencialname = 'Admin'
+                existsUser.secret = bcrypt.hashSync(data.secret, 10);
+                existsUser.id = 0;
+            } else {
+                existsUser = await this.userService.getByEmail(data.email);
+            }
+            const name = existsUser.preferencialname || existsUser.nickname || existsUser.fullname
+            if (existsUser && existsUser.email != null) {
+                if (await bcrypt.compare(data.secret, existsUser.secret)) {
+                    delete existsUser.secret
+                    const secret = process.env.SERVER_SECRET_TOKEN || 'Currículo→Único';
+                    const token = jwt.sign({
+                        id: existsUser.id,
+                        email: existsUser.email,
+                        role: existsUser.role,
+                        name: name,
+                    }, secret, { expiresIn: '2h' });
 
-                        console.log(`\n${existsUser['role']} ${existsUser['email']} acaba de fazer login no sistema`);
-                        console.log("x-access-token:", token, '\n');
+                    console.log(`\n${existsUser['role']} ${existsUser['email']} acaba de fazer login no sistema`);
+                    console.log("x-access-token:", token, '\n');
 
-                        res.status(200).json({
-                            auth: true,
-                            _id: data.id,
-                            email: data.email,
-                            expiresIn: '2h',
-                            token: token
-                        });
-
-                    } else {
-                        console.log('Senha incorreta')
-                        res.status(401).json({ auth: false, message: 'Email ou senha não confere' });
-                    }
+                    res.status(200).json({
+                        auth: true,
+                        _id: data.id,
+                        email: data.email,
+                        expiresIn: '2h',
+                        role: existsUser['role'],
+                        name: name,
+                        token: token
+                    });
 
                 } else {
-                    console.log('Email não encontrado')
+                    console.log('Senha incorreta')
                     res.status(401).json({ auth: false, message: 'Email ou senha não confere' });
                 }
 
-                return data;
-
-            } catch (err) {
-                res.status(500).json({ auth: false, message: err });
+            } else {
+                console.log('Email não encontrado')
+                res.status(401).json({ auth: false, message: 'Email ou senha não confere' });
             }
+
+            return data;
+
+        } catch (err) {
+            res.status(500).json({ auth: false, message: err });
         }
+    }
 
 }
 
