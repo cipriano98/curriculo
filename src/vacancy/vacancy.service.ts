73ccebd8e7b691a971884f2f20196fe5a42bd49f
cipriano98 <natancipriano98@gmail.com>
2020-12-14 20:26:05 -0300

    Ajustes

--- src/vacancy/vacancy.service.ts
+++ src/vacancy/vacancy.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Vacancy } from '@prisma/client';
-import { PrismaService } from 'src/prisma/prisma.service';
+
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class VacancyService {
