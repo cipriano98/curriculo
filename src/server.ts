d50fcff3f7926d0016448a190a94568082fac2d0
cipriano98 <natancipriano98@gmail.com>
2020-12-17 10:21:20 -0300

    Add new log

--- src/server.ts
+++ src/server.ts
@@ -47,11 +47,12 @@ export class Server {
         const server = await app.listen(process.env.PORT || 3000, '0.0.0.0', () => {
             console.clear()
             const url = process.env.NODE_ENV === 'production' ?
-                `https://curriculo-unico.herokuapp.com${globalPrefix}/health/status` :
-                `http://localhost:${server.address().port + globalPrefix}/health/status`
+                `https://curriculo-unico.herokuapp.com${globalPrefix}` :
+                `http://localhost:${server.address().port + globalPrefix}`
 
             // console.log(`\n[${chalk.bold.hex('#28f000')(process.env.npm_package_NAME.toUpperCase())}] is running in ${chalk.blue.underline(`http://localhost:${server.address().port + globalPrefix}`)}`)
             console.log(`[${process.env.SERVER_NAME.toUpperCase()}] is running in ${url}`)
+            console.log(`[${process.env.SERVER_NAME.toUpperCase()}] to see the status go to: ${url}/health/status`)
             console.log(process.env.SERVER_DESCRIPTION)
             console.log(`${new Date().toLocaleDateString()} - ${new Date().toLocaleTimeString()}\n`)
         })
